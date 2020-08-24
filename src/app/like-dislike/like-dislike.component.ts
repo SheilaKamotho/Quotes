a190e7e6089d51c14df89f1ba43243d5e467d81b
Kamotho Sheila <kamothosheila@gmail.com>
2020-08-24 05:22:12 +0300

    add like/dislike function

--- src/app/like-dislike/like-dislike.component.ts
+++ src/app/like-dislike/like-dislike.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./like-dislike.component.css']
 })
 export class LikeDislikeComponent implements OnInit {
+  like=0;
+  dislike=0;
+  likes(){
+    this.like=this.like+1;
+  }
+  dislikes(){
+    this.dislike=this.dislike+1;
+  }
 
   constructor() { }
 
