ec5c1f959fc626f3c6a8bd6362e1878cff8a358a
Kamotho Sheila <kamothosheila@gmail.com>
2020-08-24 18:51:42 +0300

    add hihlight

--- src/app/highlights.directive.ts
+++ src/app/highlights.directive.ts
@@ -1,12 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef,HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlights]'
 })
 export class HighlightsDirective {
 
-  constructor(elem:ElementRef) {
-    elem.nativeElement.style.backgroundColor="#834c69"
+  constructor(private elem:ElementRef) {}
+
+  @HostListener("click") onClicks(){
+    this.textDeco("highlight")
+  }
+
+  @HostListener("dblclick") onDoubleClicks(){
+    this.textDeco("None")
+  }
+
+    private textDeco(action:string){
+    this.elem.nativeElement.style.backgroundColor="#834c69"
    }
 
 }
