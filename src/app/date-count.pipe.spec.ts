9474fcd1d175eeaac0e10a4bfa5214e5c6bae961
Kamotho Sheila <kamothosheila@gmail.com>
2020-08-24 19:57:16 +0300

    final changes

--- src/app/date-count.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DateCountPipe } from './date-count.pipe';
-
-describe('DayCountPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DateCountPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
