323a9f05d0629e9d55d0447a6b707052f23cdb10
Kamotho Sheila <kamothosheila@gmail.com>
2020-08-25 13:00:35 +0300

    add formula for day pipe

--- src/app/day-count.pipe.ts
+++ src/app/day-count.pipe.ts
@@ -6,14 +6,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DayCountPipe implements PipeTransform {
 
   transform(value: any): number {
-    let today:Date = new Date(); //get current date and time
+    let today:Date = new Date(); 
     let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
     var dateDifference =Math.abs(value-todayWithNoTime )// returns value in milliseconds
     const secondsInADay= 86400; //60 seconds*60 minutes in an hour *24 hours
 
     var dateDifferenceSeconds=dateDifference*0.001; //converts to seconds
 
-    var dateCounter = dateDifferenceSeconds/secondsInADay;
+    var dateCounter = (dateDifferenceSeconds/secondsInADay)/7;
 
     if (dateCounter >= 1 && value > todayWithNoTime){
         return dateCounter;
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,7 @@
 export class Quote {
   showAuthor:boolean;
   showSubmittedBy:boolean;
-  constructor(public id: number, public quote: string, public author: string,public submittedBy: string,submitDate: Date ){
+  constructor(public id: number, public quote: string, public author: string,public submittedBy: string,public completeDate: Date ){
     this.showAuthor =false;
     this.showSubmittedBy=false;
   }
